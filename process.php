21a552369a58a3ef2f6b51bc5cdbf2fd72b0fd8e
jerukobs <48490051+jerukobs@users.noreply.github.com>
2022-05-20 12:43:16 +0000

    Update process.php

--- process.php
+++ process.php
@@ -4,18 +4,17 @@
     {
        $UserName = $_POST['UName'];
        $Email = $_POST['Email'];
-       $Subject = $_POST['Subject'];
        $Msg = $_POST['msg'];
 
-       if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
+       if(empty($UserName) || empty($Email) || empty($Msg))
        {
            header('location:index.php?error');
        }
        else
        {
-           $to = "admin@onlineittuts.com";
+           $to = "jerukobs@gmail.com";
 
-           if(mail($to,$Subject,$Msg,$Email))
+           if(mail($to,$Msg,$Email))
            {
                header("location:index.php?success");
            }
